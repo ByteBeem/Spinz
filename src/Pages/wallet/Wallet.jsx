149659e17ce07a12c039846287b1b2f6e611daa3
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2023-12-08 21:45:26 +0000

    modified:   src/Pages/wallet/Wallet.jsx

--- src/Pages/wallet/Wallet.jsx
+++ src/Pages/wallet/Wallet.jsx
@@ -1,22 +1,22 @@
-import "./wallet.scss";
-import "../../App.scss";
-import axios from "axios";
-import { useAuth } from "../../components/AuthContext";
-import { useNavigate } from "react-router-dom";
-import Sidebar from "../../components/Sidebar/Sidebar";
-import Navbar from "../../components/Navbar/Navbar";
-import { FiLoader } from "react-icons/fi";
-import React, {useState , useEffect} from 'react';
-import { Link } from 'react-router-dom'; 
+// Import statements
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useAuth } from '../../components/AuthContext';
+import { FiLoader } from 'react-icons/fi';
+import Sidebar from '../../components/Sidebar/Sidebar';
+import Navbar from '../../components/Navbar/Navbar';
+import './wallet.scss';
+import '../../App.scss';
 
-function Wallet  ({ showSidebar, active, closeSidebar }) {
+// Functional component Wallet
+const Wallet = ({ showSidebar, active, closeSidebar }) => {
   const { setToken } = useAuth();
   const [userData, setUserData] = useState({});
-
-  const balance = userData.balance;
+  const [loading, setLoading] = useState(false); // Add state for loading
 
   useEffect(() => {
-    const storedToken = localStorage.getItem("token");
+    const storedToken = localStorage.getItem('token');
 
     if (storedToken) {
       setToken(storedToken);
@@ -27,37 +27,40 @@ function Wallet  ({ showSidebar, active, closeSidebar }) {
   const fetchUserData = (token) => {
     setLoading(true);
     axios
-      .get("https://heavenly-onyx-bun.glitch.me/getUserData", {
+      .get('https://heavenly-onyx-bun.glitch.me/getUserData', {
         headers: { Authorization: `Bearer ${token}` },
       })
       .then((response) => {
         setUserData(response.data);
         setLoading(false);
       })
-      .catch((error) => {});
+      .catch((error) => {
+        setLoading(false); // Set loading to false in case of an error
+      });
   };
 
   return (
-    <div className="account_info">
-      {loading && (
-        <div className="overlay">
-          <FiLoader className="loading-spinner" />
-        </div>
-      )}
-
-<Sidebar active={active} closeSidebar={closeSidebar} />
-      <span>Account Balance:</span>
-
+    <div>
+      <Sidebar active={active} closeSidebar={closeSidebar} />
       <Navbar showSidebar={showSidebar} />
-      <div className="balance">{`R${balance}`}</div>
 
+      <div className="account_info">
+        {loading && (
+          <div className="overlay">
+            <FiLoader className="loading-spinner" />
+          </div>
+        )}
 
-      <Link className="form_btn" to="/withdraw">
-        Withdraw
-      </Link>
-      <Link className="form_btn" to="/deposit">
-        Deposit
-      </Link>
+        <span>Account Balance:</span>
+        <div className="balance">{`R${userData.balance}`}</div>
+
+        <Link className="form_btn" to="/withdraw">
+          Withdraw
+        </Link>
+        <Link className="form_btn" to="/deposit">
+          Deposit
+        </Link>
+      </div>
     </div>
   );
 };
