417600a48a0b2ce6916234644828c6596ca057cc
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2023-12-08 21:03:50 +0000

    modified:   src/Pages/wallet/wallet.jsx

--- src/Pages/wallet/wallet.jsx
+++ src/Pages/wallet/wallet.jsx
@@ -1,7 +1,9 @@
 import "./wallet.scss";
 import "../../App.scss";
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'; 
+
+const wallet = ({ balance }) => {
   return (
     <div className="account_info">
       <span>Account Balance:</span>
@@ -15,6 +17,6 @@ import { Link } from 'react-router-dom';
       </Link>
     </div>
   );
-
+};
 
 export default wallet;
