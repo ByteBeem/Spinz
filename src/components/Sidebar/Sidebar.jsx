95749089e3af544c7e4eeb03770752c2398817f1
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2023-12-08 10:00:22 +0000

    modified:   src/components/Sidebar/Sidebar.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,11 +4,30 @@ import { Link } from "react-router-dom";
 import { IoHome  } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { IoSettingsSharp } from "react-icons/io5";
+import { FiLoader } from "react-icons/fi";
 import { IoLogOut } from "react-icons/io5";
+import {useState} from 'react';
 
 const Sidebar = ({ active, closeSidebar }) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleLogout = () => {
+    setLoading(true);
+    
+    setTimeout(() => {
+      setLoading(false);
+      // Implement actual logout logic here
+      // ...
+      closeSidebar();
+    }, 5000);
+  };
   return (
     <aside className={`sidebar ${active}`}>
+      {loading && (
+        <div className="overlay">
+          <FiLoader className="loading-spinner" />
+        </div>
+      )}
       <div className="top">
         <h3>Spinz</h3>
         <div className="close_btn" onClick={() => closeSidebar()}>
@@ -18,9 +37,8 @@ const Sidebar = ({ active, closeSidebar }) => {
 
       <div className="middle">
         <Link className="link" to="/dashboard" onClick={() => closeSidebar()}>
-          <IoHome  className="icon" />
+          <IoHome className="icon" />
           <span>Home</span>
-          
         </Link>
 
         <Link className="link" to="/profile" onClick={() => closeSidebar()}>
@@ -33,7 +51,7 @@ const Sidebar = ({ active, closeSidebar }) => {
           <span>Settings</span>
         </Link>
 
-        <Link className="link" to="" onClick={() => closeSidebar()}>
+        <Link className="link" to="" onClick={handleLogout}>
           <IoLogOut className="icon" />
           <span>Logout</span>
         </Link>
