574d9d8c3ff0ffa6a628a72e00809acdca91d828
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2023-12-18 17:43:03 +0200

    Update Deposit.jsx

--- src/Pages/Deposit/Deposit.jsx
+++ src/Pages/Deposit/Deposit.jsx
@@ -117,10 +117,14 @@ function Deposit({ showSidebar, active, closeSidebar }) {
             <div className="info">
               <p>
                 <h2>For Capitec Users :</h2>To Deposit to your account , make a
-                trasnfer to account <b>2054670215</b> and use your name and
+                transfer to account <b>2054670215</b> and use your name and
                 surname as reference{" "}
               </p>
             </div>
+
+            <p>
+                <b>Other banks use the option below :</b>
+              </p>
 
             <div className="deposit_form">
               <h3>Deposit Funds</h3>
