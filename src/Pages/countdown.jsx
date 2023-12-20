74b7157dfabf560b7edbc2c033dcd69e20af92f8
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2023-12-20 15:46:34 +0200

    Update countdown.jsx

--- src/Pages/countdown.jsx
+++ src/Pages/countdown.jsx
@@ -10,10 +10,10 @@ const ComingSoon = ({ showSidebar, active, closeSidebar }) => {
       <Navbar showSidebar={showSidebar} />
       <h1>Coming Soon</h1>
       <p>We're working hard to bring you something amazing. Stay tuned!</p>
-      <P>Get ready for a groundbreaking addition to our platform! 📈 Our upcoming Forex trading service brings you the opportunity to let us handle the trading for you, with profits delivered within 24 hours.</P>
-        <P>  Deposit: Start with a minimum investment of R200.
-    Sit Back: Let our automated trading system work its magic.
-    Profit: Enjoy your earnings delivered straight to your account within 24 hours.</P>
+      <p>Get ready for a groundbreaking addition to our platform! 📈 Our upcoming Forex trading service brings you the opportunity to let us handle the trading for you, with profits delivered within 24 hours.</p>
+      <p>Deposit: Start with a minimum investment of R200.
+        Sit Back: Let our automated trading system work its magic.
+        Profit: Enjoy your earnings delivered straight to your account within 24 hours.</p>
       <div className="loading-spinner"></div>
     </div>
   );
