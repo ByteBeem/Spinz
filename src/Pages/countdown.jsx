08740dbfb232d65b41ad7ae95d9a7e9b885d75c4
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2023-12-20 15:01:54 +0200

    Update countdown.jsx

--- src/Pages/countdown.jsx
+++ src/Pages/countdown.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './ComingSoon.scss';
-import Sidebar from "../components/Sidebar/Sidebar";
-import Navbar from "../components/Navbar/Navbar";
+import Sidebar from '../components/Sidebar/Sidebar';
+import Navbar from '../components/Navbar/Navbar';
 
-const ComingSoon = () => {
+const ComingSoon = ({ showSidebar, active, closeSidebar }) => {
   return (
     <div className="coming-soon-container">
+      <Sidebar active={active} closeSidebar={closeSidebar} />
+      <Navbar showSidebar={showSidebar} />
       <h1>Coming Soon</h1>
       <p>We're working hard to bring you something amazing. Stay tuned!</p>
       <div className="loading-spinner"></div>
