40de02ea2b4ad811c39daf04279c413fcacebce7
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2023-12-20 14:52:06 +0200

    Create countdown.jsx

---
+++ src/Pages/countdown.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ComingSoon.scss';
+
+const ComingSoon = () => {
+  return (
+    <div className="coming-soon-container">
+      <h1>Coming Soon</h1>
+      <p>We're working hard to bring you something amazing. Stay tuned!</p>
+      <div className="loading-spinner"></div>
+    </div>
+  );
+};
+
+export default ComingSoon;
