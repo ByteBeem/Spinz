7f86d114063318b4b85585db375474f83fe30cf6
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2023-12-21 15:16:43 +0200

    Update Login.jsx

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -79,7 +79,7 @@ function Login() {
 
     try {
       const response = await axios.post(
-        "https://warm-honored-cuticle.glitch.me/login",
+        "https://chill-emphasized-radish.glitch.me/login",
         {
           cell: cellphone,
           password: password,
