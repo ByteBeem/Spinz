18f3749b3855b5f563dae40cced0c269b30745c8
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2023-12-22 13:23:03 +0200

    Update Login.jsx

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -79,7 +79,7 @@ function Login() {
 
     try {
       const response = await axios.post(
-        "https://chill-emphasized-radish.glitch.me/login",
+        "https://server-gray-nine.vercel.app/login",
         {
           cell: cellphone,
           password: password,
