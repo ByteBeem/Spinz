3d8bd63adc737e421c9345fe90345ee68a7c4f25
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2023-12-22 19:03:28 +0200

    Update Chatbot.jsx

--- src/Pages/chatbot/Chatbot.jsx
+++ src/Pages/chatbot/Chatbot.jsx
@@ -13,122 +13,117 @@ const Chatbot = ({ showSidebar, active, closeSidebar }) => {
   const [userColor, setUserColor] = useState(null);
   const [socket, setSocket] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [showSendPicture, setShowSendPicture] = useState(false);
 
   useEffect(() => {
-    const initializeChat = async () => {
-      try {
-        const token = localStorage.getItem("token");
+    const token = localStorage.getItem("token");
 
-        if (!token) return;
+    if (token) {
+      axios.post("https://mousy-mirror-tick.glitch.me/userChat", { token })
+        .then((response) => {
+          const { name, messages } = response.data;
 
-        const response = await axios.post("https://mousy-mirror-tick.glitch.me/userChat", { token });
-        const { name, messages } = response.data;
+          localStorage.setItem("user_name", name);
 
-        localStorage.setItem("user_name", name);
+          const sortedMessages = messages.sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp));
 
-        const sortedMessages = messages.sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp));
-
-        setMessages(sortedMessages);
-        setLoading(false);
-
-        const newSocket = io("https://mousy-mirror-tick.glitch.me/");
-
-        newSocket.on("user-color", ({ color }) => {
-          setUserColor(color);
-        });
-
-        newSocket.on("connect", () => {
+          setMessages(sortedMessages);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error("Error fetching user name:", error);
           setLoading(false);
         });
 
-        setSocket(newSocket);
+      const newSocket = io("https://mousy-mirror-tick.glitch.me/");
 
-        return () => {
-          newSocket.disconnect();
-        };
-      } catch (error) {
-        console.error("Error initializing chat:", error);
+      newSocket.on("user-color", ({ color }) => {
+        setUserColor(color);
+      });
+
+      newSocket.on("connect", () => {
         setLoading(false);
-      }
-    };
+      });
 
-    initializeChat();
+      setSocket(newSocket);
+
+      return () => {
+        newSocket.disconnect();
+      };
+    }
   }, []);
 
   useEffect(() => {
-    if (socket) {
-      socket.on("chat-message", (message) => {
-        setMessages((prevMessages) => [...prevMessages, message]);
-      });
+    if (!socket) return;
 
-      return () => {
-        socket.off("chat-message");
-      };
-    }
+    socket.on("chat-message", (message) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+
+    return () => {
+      socket.off("chat-message");
+    };
   }, [socket]);
 
   const handleSendMessage = () => {
-    if (userInput.trim() !== "") {
-      if (socket) {
-        const name = localStorage.getItem("user_name");
+  if (userInput.trim() !== "") {
+    if (socket) {
+      const name = localStorage.getItem("user_name");
 
-        const messageType = userInput.startsWith("data:image") ? "image" : "text";
-
+      if (userInput.startsWith('data:image')) {
+        // Image message
         socket.emit("user-message", {
           message: {
-            type: messageType,
+            type: 'image',
             content: userInput,
             name: name,
           },
         });
-
-        setUserInput("");
-      }
-    }
-  };
-
-  const handleImageUpload = () => {
-    if (socket) {
-      const fileInput = document.getElementById("imageUpload");
-      const imageFile = fileInput.files[0];
-
-      if (imageFile) {
-        setSelectedImage(URL.createObjectURL(imageFile));
-        setShowSendPicture(true);
       } else {
-        setShowSendPicture(false);
-      }
-    }
-  };
-
-  const handleSendImage = async () => {
-    try {
-      if (socket && selectedImage) {
-        const name = localStorage.getItem("user_name");
-
-        const formData = new FormData();
-        formData.append("image", selectedImage);
-
-        const response = await axios.post("https://mousy-mirror-tick.glitch.me/upload", formData);
-
-        const imageUrl = response.data.imageUrl;
+        // Text message
         socket.emit("user-message", {
           message: {
-            type: "image",
-            content: imageUrl,
+            type: 'text',
+            text: userInput,
             name: name,
           },
         });
-
-        setSelectedImage(null);
-        setShowSendPicture(false);
       }
-    } catch (error) {
-      console.error("Error uploading image:", error);
+
+      setUserInput("");
     }
-  };
+  }
+};
+
+
+
+  const handleImageUpload = () => {
+  if (socket) {
+    const name = localStorage.getItem('user_name');
+    const fileInput = document.getElementById('imageUpload');
+    const imageFile = fileInput.files[0];
+
+    if (imageFile) {
+      const formData = new FormData();
+      formData.append('image', imageFile);
+
+      // Use fetch to send the image to the server
+      fetch('https://mousy-mirror-tick.glitch.me/upload', {
+        method: 'POST',
+        body: formData,
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data); // Log the server response
+          // Handle the server response as needed
+        })
+        .catch((error) => {
+          console.error('Error uploading image:', error);
+        });
+    }
+  }
+};
+
+
 
   const handleVoiceNote = () => {
     // Implement voice note recording logic here
@@ -145,85 +140,53 @@ const Chatbot = ({ showSidebar, active, closeSidebar }) => {
           {loading && <div className="overlay">Connecting...</div>}
 
           <ul className="chat-messages">
-            {messages.map((message, index) => (
+{messages.map((message, index) => (
               <li
                 key={index}
                 style={{
                   backgroundColor:
                     message.username === socket.id
                       ? "#3498db"
-                      : message.color || userColor,
+                      : message.color,
                   alignSelf:
                     message.username === socket.id ? "flex-end" : "flex-start",
                 }}
               >
-                <small>{message.username} : </small> {message.text}
+<small>{message.username} : </small> {message.text}
               </li>
             ))}
+
           </ul>
 
           <div className="user-input">
-            {!showSendPicture ? (
-              <>
-                <form action="/upload" method="post" encType="multipart/form-data" onSubmit={(e) => e.preventDefault()}>
-                  <label htmlFor="imageUpload">
-                    <FontAwesomeIcon icon={faCamera} />
-                    <input
-                      type="file"
-                      id="imageUpload"
-                      name="image"
-                      accept="image/*"
-                      onChange={handleImageUpload}
-                    />
-                  </label>
-                </form>
+            <div className="input-icons">
+              
+<label htmlFor="imageUpload" className="icon">
+  <FontAwesomeIcon icon={faCamera} />
+  <input
+    type="file"
+    id="imageUpload"
+    accept="image/*"
+    onChange={handleImageUpload}
+  />
+</label>
 
-                <textarea
-                  className="user_msg"
-                  placeholder="Type your message..."
-                  value={userInput}
-                  onChange={(e) => setUserInput(e.target.value)}
-                ></textarea>
 
-                <button
-                  style={{
-                    marginLeft: "10px",
-                    height: "60px",
-                    marginTop: "35px",
-                  }}
-                  onClick={handleSendMessage}
-                >
-                  Send
-                </button>
+              <textarea
+                className="user_msg"
+                placeholder="Type your message..."
+                value={userInput}
+                onChange={(e) => setUserInput(e.target.value)}
+              ></textarea>
+            </div>
+<button style={{ marginRight: '10px' , height: '60px' , marginTop:'35px' }} onClick={handleSendMessage}>
+  <FontAwesomeIcon icon={faMicrophone} />
+  Voice
+</button>
+<button style={{ marginLeft: '10px' ,  height: '60px' , marginTop:'35px'}} onClick={handleSendMessage}>
+  Send
+</button>
 
-                <button
-                  style={{
-                    marginLeft: "10px",
-                    height: "60px",
-                    marginTop: "35px",
-                  }}
-                  onClick={handleVoiceNote}
-                >
-                  <FontAwesomeIcon icon={faMicrophone} />
-                  Voice
-                </button>
-              </>
-            ) : (
-              <>
-                {selectedImage && (
-                  <button
-                    style={{
-                      marginRight: "10px",
-                      height: "60px",
-                      marginTop: "35px",
-                    }}
-                    onClick={handleSendImage}
-                  >
-                    Send Picture
-                  </button>
-                )}
-              </>
-            )}
           </div>
         </div>
       </div>
