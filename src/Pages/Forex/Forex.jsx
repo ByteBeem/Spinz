a927708a0e69582f29f596eae2587d0f74fd9f2c
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2023-12-29 12:51:44 +0200

    Update Forex.jsx

--- src/Pages/Forex/Forex.jsx
+++ src/Pages/Forex/Forex.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import './Forex.scss';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import Navbar from '../../components/Navbar/Navbar';
 import ForexChart from './ForexChart';
 
 const Forex = ({ showSidebar, active, closeSidebar }) => {
+  const [amount, setAmount] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+
+
+  const handleDeposit = () => {
+    setError("");
+    setMessage("");
+    setLoading(true);
+
+    const token = localStorage.getItem("token");
+    console.log("token",token);
+    if (!token) {
+  setError("Token not found , Go log in again.");
+  setLoading(false);
+  return;
+}
+
+    if (isNaN(amount) || amount <= 0 ) {
+      setError("Invalid amount");
+      setLoading(false);
+      return;
+    }
+
+    if (amount < 200) {
+      setError("Minimum amount is R200");
+      setLoading(false);
+      return;
+    }
+
+  }
+
+
   return (
     <div className="forex">
       <Sidebar active={active} closeSidebar={closeSidebar} />
@@ -16,6 +50,27 @@ const Forex = ({ showSidebar, active, closeSidebar }) => {
 
           <ForexChart />
           <h3>Let's Handle The Trading for you!</h3>
+          <div className="deposit_form">
+              <div>
+                <label>Amount</label>
+                <br />
+                <input
+                  type="number"
+                  value={amount}
+                  onChange={(e) => setAmount(e.target.value)}
+                  inputMode="numeric"
+                />
+                <button
+                  className="form_btn"
+                  onClick={handleDeposit}
+                  disabled={loading}
+                >
+                  {loading ? "Processing..." : "Begin"}
+                </button>
+                {message && <p className="success-message">{message}</p>}
+                {error && <p className="error-message">{error}</p>}
+                </div>
+                </div>
         </div>
       </div>
     </div>
