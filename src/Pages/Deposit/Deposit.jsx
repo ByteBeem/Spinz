fa0116af0d55e8d368f7e89173bce1da7ed45da0
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2024-01-16 20:42:43 +0200

    Update Deposit.jsx

--- src/Pages/Deposit/Deposit.jsx
+++ src/Pages/Deposit/Deposit.jsx
@@ -3,10 +3,8 @@ import axios from "axios";
 import "./Deposit.scss";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Navbar from "../../components/Navbar/Navbar";
-import {PayPalScriptProvider} from "@paypal/react-paypal-js";
-import { PayPalButtons } from "@paypal/react-paypal-js";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import { Link, useNavigate } from "react-router-dom";
-
 
 function Deposit({ showSidebar, active, closeSidebar }) {
   const [amount, setAmount] = useState("");
@@ -15,15 +13,13 @@ function Deposit({ showSidebar, active, closeSidebar }) {
   const [message, setMessage] = useState("");
   const [error, setError] = useState("");
   const [showModal, setShowModal] = useState(false);
-  const [Currentbalance, setCurrentBalance] = useState("0.00");
+  const [currentBalance, setCurrentBalance] = useState("0.00");
 
   const token = localStorage.getItem("token");
-
   const navigate = useNavigate();
 
   const fetchBalance = async () => {
     try {
-      // Send the token as an Authorization header to the server
       const response = await axios.get(
         "https://spinz-server-100d0276d968.herokuapp.com/balance",
         {
@@ -34,13 +30,12 @@ function Deposit({ showSidebar, active, closeSidebar }) {
       );
 
       if (response.status === 206) {
-        alert("Token Expired Login again!");
+        alert("Token Expired. Please log in again!");
       } else {
         setCurrentBalance(response.data.balance);
       }
     } catch (error) {
       console.error("Error fetching balance:", error);
-    } finally {
     }
   };
 
@@ -49,7 +44,6 @@ function Deposit({ showSidebar, active, closeSidebar }) {
   }, [token]);
 
   useEffect(() => {
-    // Fetch user's balance when the component mounts
     const token = localStorage.getItem("token");
     if (token) {
       axios
@@ -74,12 +68,12 @@ function Deposit({ showSidebar, active, closeSidebar }) {
     setLoading(true);
 
     const token = localStorage.getItem("token");
-    console.log("token",token);
+
     if (!token) {
-  setError("Token not found , Go log in again.");
-  setLoading(false);
-  return;
-}
+      setError("Token not found. Please log in again.");
+      setLoading(false);
+      return;
+    }
 
     if (isNaN(amount) || amount <= 0) {
       setError("Invalid amount");
@@ -101,8 +95,7 @@ function Deposit({ showSidebar, active, closeSidebar }) {
       )
       .then((response) => {
         setMessage(`Redirecting...`);
-        //window.location.href = response.data.redirectUrl;
-setShow(true);
+        setShow(true);
         setHide(true);
         setAmount("");
       })
@@ -114,102 +107,79 @@ setShow(true);
       });
   };
 
- 
-
   const [show, setShow] = useState(false);
   const [hide, setHide] = useState(false);
-  const [success , setSuccess] = useState(false);
-  const [errorMessage , setErrorMessage] = useState(false);
-   const [orderId , setOrderId] = useState(false);
 
-  const createOrder = (data , actions) => {
-    setShowModal(true); 
-    return actions.order.create({
-      purchase_units : [
-        {
-          description : 'depositToSpinz',
-          amount : {
-            currency_code : 'USD',
-            value : 20
+  const createOrder = (data, actions) => {
+    setShowModal(true);
+    return actions.order
+      .create({
+        purchase_units: [
+          {
+            description: "depositToSpinz",
+            amount: {
+              currency_code: "USD",
+              value: 20,
+            },
           },
-
-        },
         ],
-      application_context: {
-        shipping_preference:'NO_SHIPPING'
-      }
-    })
-    .then ((orderID) => {
-      setOrderId(orderID)
-      return orderID
-    })
-
+        application_context: {
+          shipping_preference: "NO_SHIPPING",
+        },
+      })
+      .then((orderID) => {
+        setOrderId(orderID);
+        return orderID;
+      });
   };
 
-  const onApprove = (data , actions) => {
-    return actions.order.capture().then (function (details ) {
-      const {payer} =details;
+  const onApprove = (data, actions) => {
+    return actions.order.capture().then((details) => {
+      const { payer } = details;
       setSuccess(true);
-      
-
-    })
-
+    });
   };
 
-  const onError = (data , actions) => {
-    
-setErrorMessage("Something went wrong");
-  }
+  const onError = (data, actions) => {
+    setErrorMessage("Something went wrong");
+  };
 
   return (
     <div className="deposit">
       <Sidebar active={active} closeSidebar={closeSidebar} />
-
       <div className="deposit_container">
         <Navbar showSidebar={showSidebar} />
-
         <div className="content">
-          <PayPalScriptProvider 
-
+          <PayPalScriptProvider
             options={{
-              'client-id':'Aft3OCQujzt42-4_EAtWyIeLnZ-RsLynG4BbhVztRHfKHLe2OxPEl3a1HakXW1b4ASv1YCsUaOjLgm-A'
+              "client-id": "Aft3OCQujzt42-4_EAtWyIeLnZ-RsLynG4BbhVztRHfKHLe2OxPEl3a1HakXW1b4ASv1YCsUaOjLgm-A",
             }}
-
-            >
-
-
-          {show ? (
-      <PayPalButtons style={{ layout: "vertical" }} createOrder={createOrder} onApprove={onApprove}   onError={onError}/>
-       
-          ) : null}
-            </PayPalScriptProvider>
+          >
+            {show ? (
+              <PayPalButtons style={{ layout: "vertical" }} createOrder={createOrder} onApprove={onApprove} onError={onError} />
+            ) : null}
+          </PayPalScriptProvider>
 
           <div className="middle">
-           
-
             <div className="deposit_form">
-              {Hide ? (
-              <h3>Deposit Funds</h3>
-              <div>
-                <label>Deposit Amount</label>
-                <br />
-                <input
-                  type="number"
-                  value={amount}
-                  onChange={(e) => setAmount(e.target.value)}
-                  inputMode="numeric"
-                />
-                <button
-                  className="form_btn"
-                  onClick={handleDeposit}
-                  disabled={loading}
-                >
-                  {loading ? "Processing..." : "Deposit"}
-                </button>
-                {message && <p className="success-message">{message}</p>}
-                {error && <p className="error-message">{error}</p>}
+              {hide ? (
+                <div>
+                  <h3>Deposit Funds</h3>
+                  <label>Deposit Amount</label>
+                  <br />
+                  <input
+                    type="number"
+                    value={amount}
+                    onChange={(e) => setAmount(e.target.value)}
+                    inputMode="numeric"
+                  />
+                  <button className="form_btn" onClick={handleDeposit} disabled={loading}>
+                    {loading ? "Processing..." : "Deposit"}
+                  </button>
+                  {message && <p className="success-message">{message}</p>}
+                  {error && <p className="error-message">{error}</p>}
+                </div>
               ) : null}
-              </div>
             </div>
           </div>
         </div>
