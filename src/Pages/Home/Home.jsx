ccd721b99c6e3c6238d6680b4d074c4dad07ebcd
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2024-02-07 11:09:33 +0000

    new

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import Navbar from "../../components/Navbar/Navbar";
 import Sidebar from "../../components/Sidebar/Sidebar";
@@ -10,27 +10,13 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./Home.scss";
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
+const Home = ({ showSidebar, active, closeSidebar }) => {
+  const [loading, setLoading] = useState(false);
+  const [betAmountInput, setBetAmountInput] = useState("");
+  const navigate = useNavigate();
+  const token = localStorage.getItem('token');
 
-    this.state = {
-      loading: false,
-      betAmountInput: "",
-    };
-
-    this.navigate = useNavigate();
-    this.token = localStorage.getItem('token');
-    this.settings = {
-      dots: false,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-    };
-  }
-
-  handlePlayClick = async (id) => {
+  const handlePlayClick = async (id) => {
     const storedToken = localStorage.getItem("token");
 
     if (id === 2) {
@@ -41,10 +27,10 @@ class Home extends Component {
         return;
       }
 
-      this.setState({ betAmountInput: userBetAmount });
+      setBetAmountInput(userBetAmount);
     }
 
-    this.setState({ loading: true });
+    setLoading(true);
 
     try {
       const headers = { Authorization: `Bearer ${storedToken}` };
@@ -55,8 +41,8 @@ class Home extends Component {
         case 5:
         case 7:
           const response = await axios.post(
-            `https://spinz-server-100d0276d968.herokuapp.com/${this.getGamePath(id)}`,
-            this.getGameData(id),
+            `https://spinz-server-100d0276d968.herokuapp.com/${getGamePath(id)}`,
+            getGameData(id),
             { headers }
           );
           if (response.status === 400) {
@@ -75,13 +61,13 @@ class Home extends Component {
           console.log("Unknown game id");
       }
     } catch (error) {
-      alert("Something went wrong , try again later");
+      alert("Something went wrong, try again later");
     } finally {
-      this.setState({ loading: false });
+      setLoading(false);
     }
   };
 
-  getGamePath = (id) => {
+  const getGamePath = (id) => {
     const paths = {
       1: "slot",
       2: "startGame",
@@ -91,46 +77,41 @@ class Home extends Component {
     return paths[id] || "";
   };
 
-  getGameData = (id) => {
+  const getGameData = (id) => {
     if (id === 2) {
-      return { betAmount: this.state.betAmountInput };
+      return { betAmount: betAmountInput };
     }
     return {};
   };
 
-  render() {
-    const { showSidebar, active, closeSidebar } = this.props;
-    const { loading } = this.state;
-
-    return (
-      <div className="home">
-        <Sidebar active={active} closeSidebar={closeSidebar} />
-        <div className="home_container">
-          <Navbar showSidebar={showSidebar} />
-          <div className="content">
-            <div className="games_slider">
-              <div className="div">
-                <Slider {...this.settings}>
-                  {Games.map(({ id, title, img }) => (
-                    <div key={id} className="game_box">
-                      <img src={img} alt="" className="game_img" />
-                      <div className="title">{title}</div>
-                      <div
-                        className="form_btn"
-                        onClick={() => this.handlePlayClick(id)}
-                      >
-                        {loading ? "Loading..." : "Play"}
-                      </div>
+  return (
+    <div className="home">
+      <Sidebar active={active} closeSidebar={closeSidebar} />
+      <div className="home_container">
+        <Navbar showSidebar={showSidebar} />
+        <div className="content">
+          <div className="games_slider">
+            <div className="div">
+              <Slider {...settings}>
+                {Games.map(({ id, title, img }) => (
+                  <div key={id} className="game_box">
+                    <img src={img} alt="" className="game_img" />
+                    <div className="title">{title}</div>
+                    <div
+                      className="form_btn"
+                      onClick={() => handlePlayClick(id)}
+                    >
+                      {loading ? "Loading..." : "Play"}
                     </div>
-                  ))}
-                </Slider>
-              </div>
+                  </div>
+                ))}
+              </Slider>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Home;
