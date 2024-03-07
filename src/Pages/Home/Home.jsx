6e8f41a710f430fc18f6c082a8d3057dcf17c458
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2024-03-07 23:47:47 +0200

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import Slider from "react-slick";
 import Games from "../../Data/Games";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import aviatorImage from "../../path/to/aviator.png"; // Import the aviator image
 import "./Home.scss";
 
 class Home extends Component {
@@ -23,7 +24,7 @@ class Home extends Component {
       infinite: true,
       speed: 500,
       slidesToShow: 4,
-      slidesToScroll: 1,
+      slidesToScroll: 1, 
     };
   }
 
@@ -52,7 +53,7 @@ class Home extends Component {
         case 5:
         case 7:
           const response = await axios.post(
-            `https://spinzserver-e34cd148765a.herokuapp.com/${this.getGamePath(id)}`,
+            `https://spinz-server-100d0276d968.herokuapp.com/${this.getGamePath(id)}`,
             this.getGameData(id),
             { headers }
           );
@@ -72,7 +73,7 @@ class Home extends Component {
           console.log("Unknown game id");
       }
     } catch (error) {
-      alert("Something went wrong, try again later");
+      alert("Something went wrong , try again later");
     } finally {
       this.setState({ loading: false });
     }
@@ -105,11 +106,16 @@ class Home extends Component {
         <div className="home_container">
           <Navbar showSidebar={showSidebar} />
           <div className="content">
+            {/* Aviator Image Section */}
+            <div className="aviator_image">
+              <img src={aviatorImage} alt="Aviator" />
+            </div>
+            {/* Games Slider Section */}
             <div className="games_slider">
               <Slider {...this.settings}>
                 {Games.map(({ id, title, img }) => (
                   <div key={id} className="game_box">
-                    <img src={img} alt={title} className="game_img" />
+                    <img src={img} alt="" className="game_img" />
                     <div className="title">{title}</div>
                     <div
                       className="form_btn"
